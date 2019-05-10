333ccd05b2ef98c0789eed055ce17bf317276f28
matiKloster <matikloster2@gmail.com>
2019-05-10 19:12:38 -0300

    asd

--- app/src/main/java/external/ExternalArtist.kt
+++
@@ -1,7 +0,0 @@
-package ayds.spotisong.aurora.otherdetails.model.repository.external
-
-interface ExternalArtist {
-    var artistBio: String?
-    var artistImagePath: String?
-    var source: Int
-}
--- app/src/main/java/external/OtherInfoExternalService.kt
+++
@@ -1,5 +0,0 @@
-package ayds.spotisong.aurora.otherdetails.model.repository.external
-
-interface OtherInfoExternalService {
-    fun getInfoFromService(artistName: String): ExternalArtist
-}
